6e9a007a9daec6eab19a144a2d9c2313aab2b939
martisya <martisyahalid@gmail.com>
2020-01-14 11:09:07 +0800

    Menu app- Item Price cut off

--- App/Home/ProductCell.js
+++ App/Home/ProductCell.js
@@ -15,18 +15,18 @@ import {
   TouchableOpacity
 } from "react-native";
 import React from "react";
-import { alpha, fontAlpha ,windowWidth} from "../Common/size";
-import {TITLE_FONT, NON_TITLE_FONT} from "../Common/common_style";
+import { alpha, fontAlpha, windowWidth } from "../Common/size";
+import { TITLE_FONT, NON_TITLE_FONT } from "../Common/common_style";
 
 export default class ProductCell extends React.Component {
   constructor(props) {
     super(props);
   }
 
-  componentDidMount() {}
+  componentDidMount() { }
 
   onProductCellPress = () => {
-   // this.props.onCellPress(this.props.item, this.props.index);
+    // this.props.onCellPress(this.props.item, this.props.index);
   };
 
   onAddPressed = () => {
@@ -55,24 +55,24 @@ export default class ProductCell extends React.Component {
     return (
       <TouchableWithoutFeedback onPress={this.onProductCellPress}>
         <View navigation={this.props.navigation} style={styles.productcell}>
-        
-              <Image
-                source={{ uri: this.props.productimage }}
-                style={styles.productimageImage}
-              />
-            
-          
+
+          <Image
+            source={{ uri: this.props.productimage }}
+            style={styles.productimageImage}
+          />
+
+
           <View style={styles.productDetail}>
             <Text adjustsFontSizeToFit numberOfLines={3} style={styles.titleText}>{this.props.productname}</Text>
             <Text style={styles.priceText}>
               ${parseFloat(this.props.productprice).toFixed(2)}
             </Text>
-        </View>
-        {this.props.productstatus != null && this.props.productstatus.length > 0 ? 
-                  <View style={styles.soldView}>
-                    <Text style={styles.soldtextText}>{this.props.productstatus}</Text>
-                  </View>
-                : null }
+          </View>
+          {this.props.productstatus != null && this.props.productstatus.length > 0 ?
+            <View style={styles.soldView}>
+              <Text style={styles.soldtextText}>{this.props.productstatus}</Text>
+            </View>
+            : null}
 
         </View>
       </TouchableWithoutFeedback>
@@ -82,14 +82,14 @@ export default class ProductCell extends React.Component {
 
 const styles = StyleSheet.create({
   productDetail: {
-backgroundColor:"white",
+    backgroundColor: "white",
   },
   row: {
     flex: 1,
     flexDirection: "row",
     width: "100%",
     marginLeft: 2 * alpha
-  }, 
+  },
   lineText: {
     backgroundColor: "black",
     width: "70%",
@@ -98,27 +98,27 @@ backgroundColor:"white",
   productcell: {
     backgroundColor: "rgba(255, 255, 255, 1)",
     // backgroundColor: "yellow",
-    width: (windowWidth-90)/2 - 20 *alpha,
-    height: 160 * alpha,
+    width: (windowWidth - 90) / 2 - 20 * alpha,
+    height: 175 * alpha,
     flexDirection: "column",
     alignItems: "center",
-    marginLeft:1*alpha,
+    marginLeft: 1 * alpha,
     marginTop: 10 * alpha,
     marginBottom: 5 * alpha
   },
-  productimageImage: {    
+  productimageImage: {
     resizeMode: "cover",
     backgroundColor: "white",
     width: 130 * alpha,
     height: 130 * alpha,
     marginLeft: 0 * alpha,
-    marginBottom: 5 * alpha,
+    marginBottom: 10 * alpha,
   },
   soldView: {
     position: "absolute",
     alignSelf: "center",
     width: 74 * alpha,
-    bottom: 30 * alpha,
+    bottom: 35 * alpha,
     height: 22 * alpha,
     justifyContent: "center",
     alignItems: "center"
@@ -126,8 +126,8 @@ backgroundColor:"white",
   soldtextText: {
     backgroundColor: "rgb(0, 178, 227)",
     color: "white",
-    paddingLeft: 5*alpha,
-    paddingRight: 5*alpha,
+    paddingLeft: 5 * alpha,
+    paddingRight: 5 * alpha,
     fontFamily: TITLE_FONT,
     fontSize: 8 * fontAlpha,
     fontStyle: "normal",
@@ -140,7 +140,7 @@ backgroundColor:"white",
     fontSize: 10 * fontAlpha,
     textAlign: "center",
     backgroundColor: "rgba(255, 255, 255, 1)"
-  },  
+  },
   descriptionText: {
     backgroundColor: "transparent",
     opacity: 0.39,
@@ -163,7 +163,7 @@ backgroundColor:"white",
     textAlign: "center",
     marginTop: 3 * alpha
   },
- 
+
   numberofitemText: {
     color: "rgb(255, 251, 251)",
     fontFamily: NON_TITLE_FONT,
